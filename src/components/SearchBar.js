01cf3244d6cc8cad36dc76f74bf70c8f917c5580
Mitch Lum <mklum88@gmail.com>
2021-10-17 11:15:12 -0700

    connected Semantic UI and added styling to Search Bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+  onInputChange = (event) => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   render() {
-    return <div>Search Bar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <div onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
