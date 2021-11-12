3f2c9434fad85be44fe8215194826979115e1508
Mitch Lum <mklum88@gmail.com>
2021-11-11 16:03:14 -0800

    finish app with grid styling

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBf-mBYkmKoyfGEBInvrtMxeZuMQBQGiho";
+const KEY = "AIzaSyDPUWBzoL76ZaXN2BhWfQDqpwM41jaW9FU";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
